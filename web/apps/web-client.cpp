87006801bce576b3114be15bbe42d0731b84813a
AR Visions <ar.hyperspace@gmail.com>
2021-11-22 19:34:19 -0700

    update test

--- web/apps/web-client.cpp
+++ web/apps/web-client.cpp
@@ -2,19 +2,16 @@
 #include <ux/ux.hpp>
 
 /// fetch a json resource, with quotes from anton petrov
+/// to run, set cwd
 int main(int c, const char *v[]) {
     var  defs = Map {
-        { "url", "https://ar-visions.github.io/wonderful-resource.json" }
+        { "url", "https://ar-visions.github.io/test-resource.json" }
     };
     Args args = var::args(c, v, defs, "url");
     ///
-    Web::json(args["url"]).then([&](var &v_msg) {
-        Web::Message msg = v_msg;
-        bool b_value = msg.content["stay-wonderful"];
-        if (b_value)
-            console.log("expected result: {0}", {b_value});
-        else
-            console.fatal("space out");
+    Web::json(args["url"]).then([&](Web::Message msg) {
+        if (msg.content["wonderful"])
+            console.log("expected result");
         ///
     }).except([&](var &err) {
         console.log("request failure: {0}", {err});
